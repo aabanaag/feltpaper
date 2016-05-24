431bc515c6ddb25a769046f0131b9c7864c9e95b
Jong Banaag <aabanaag@gmail.com>
2016-05-24 22:14:29 +0800

    [modified] gulpfile to use individual plugin instead of gulp-load-plugin

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,50 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var plugins = require('gulp-load-plugins')();
+var coffee = require('gulp-coffee');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+
+var path = {
+  scripts: './src/**/*.coffee',
+  styles: './src/**/*.scss',
+  html: './src/**/*.html'
+}
 
 //GULP TASK
 gulp.task('script', function () {
-  return gulp.src('./src/**/*.coffee')
-  .pipe(plugins.coffee({
+  return gulp.src(path.scripts)
+  .pipe(coffee({
     bare: true
   }).on('error', gutil.log))
   .pipe(gulp.dest('./dist/js'))
+  .pipe(connect.reload())
 });
 
 gulp.task('style', function () {
-  return gulp.src('./src/**/*.scss')
-  .pipe(plugins.sass().on('error', sass.logError))
-  .pipe(gulp.des('./dist/css'))
+  return gulp.src(path.styles)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(connect.reload())
 });
 
-gulp.task('default', ['script', 'style']);
+gulp.task('html', function () {
+  return gulp.src(path.html)
+  .pipe(gulp.dest('./dist'))
+  .pipe(connect.reload())
+})
+
+gulp.task('watch', function () {
+  gulp.watch(path.scripts, ['script']);
+  gulp.watch(path.styles, ['style']);
+  gulp.watch(path.html, ['html'])
+});
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+})
+
+gulp.task('default', ['connect', 'script', 'style', 'html', 'watch']);
